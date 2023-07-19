d05dceb3a6f7c415dc799f9e6e2cb4547f85e938
Wibo98 <nabileelmorchid@gmail.com>
2023-07-19 12:14:34 +0200

    redone

--- models/product.js
+++ models/product.js
@@ -36,18 +36,3 @@ module.exports = class Product {
         getProductFromFile(cb);
 }
 };
-const products = [];
-
-module.exports = class Product {
-    constructor(t){
-        this.title = t;
-    }
-
-    save(){
-        products.push(this);
-    }
-
-    static fetchAll(){
-            return products;
-    }
-}
