ce3ab684453fb0e1b038e06638f8b079d4c2a98d
Wibo98 <nabileelmorchid@gmail.com>
2023-09-18 12:27:32 +0200

    cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
+
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -35,10 +37,12 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res , next) => {
-  const prodId = req.body.productId ;
-  console.log(prodId);
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect('/cart');
-}
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
